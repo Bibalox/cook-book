2d56536380089faae1701260cfbe979f86297962
JB Bal <bal@abvent.fr>
2023-03-30 11:35:55 +0200

    🚧 ✨ First draft of the PWA support

--- src/components/BackgroundImage.vue
+++ src/components/BackgroundImage.vue
@@ -28,6 +28,7 @@ defineProps({
   overflow: hidden;
   position: absolute;
   top: 0;
+  width: 100%;
   z-index: -1;
 
   &__image {
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,39 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        '/icons/favicon-32x32.png',
+        '/icons/apple-touch-icon.png',
+        '/icons/safari-pinned-tab.svg'
+      ],
+      manifest: {
+        name: 'Cook book',
+        short_name: 'CookBook',
+        description: 'Un moyen simple de conserver mes recettes préférées',
+        theme_color: '#FAFAFA',
+        icons: [
+          {
+            src: '/icons/android-chrome-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/android-chrome-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
