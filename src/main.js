2003d126e922c8f83a0d4a954fe8828939a23479
JB Bal <bal@abvent.fr>
2023-03-31 12:52:29 +0200

    💚 Fix the fix

--- src/main.js
+++ src/main.js
@@ -14,3 +14,9 @@ if (darkTheme.matches) {
 } else {
   document.documentElement.setAttribute('data-theme', 'light')
 }
+
+try {
+  await navigator.wakeLock.request('screen')
+} catch (err) {
+  console.error('Wakelock cannot be activated')
+}
